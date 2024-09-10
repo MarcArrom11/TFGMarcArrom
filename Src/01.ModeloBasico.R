41a969d246638e0b40db727a555168a3fc72f422
Cruzalirio <nelsonaliriocruz@gmail.com>
2024-09-10 14:35:35 +0200

    Alirio1.0

---
+++ Src/01.ModeloBasico.R
@@ -0,0 +1,16 @@
+###################################
+## librerias
+
+library(tidyverse)
+library(survival)
+library(readxl)
+
+
+#### Cargar datos
+Arkansas <- read_excel("Input/Arkansas.xlsx")
+Arkansas$ASPC = as.numeric(Arkansas$ASPC)
+Arkansas$BMPC = as.numeric(Arkansas$BMPC)
+
+
+modelo = survreg(Surv(OS_Time, OS_Censor)~B2M+ALB+SEX+ASPC+BMPC, data=Arkansas, dist = "weibull")
+summary(modelo)
