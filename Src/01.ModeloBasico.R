b0a9fa926aa3ce49c4a8a2aa4a1d6a341b790300
Cruzalirio <nelsonaliriocruz@gmail.com>
2024-09-10 14:46:09 +0200

    Alirio2.0

--- Src/01.ModeloBasico.R
+++ Src/01.ModeloBasico.R
@@ -14,3 +14,5 @@ Arkansas$BMPC = as.numeric(Arkansas$BMPC)
 
 modelo = survreg(Surv(OS_Time, OS_Censor)~B2M+ALB+SEX+ASPC+BMPC, data=Arkansas, dist = "weibull")
 summary(modelo)
+
+save(modelo, file="Output/modelo1.RData")
