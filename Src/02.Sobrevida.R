e935203a83166a073384af0bc7631b68a453bc3c
Cruzalirio <nelsonaliriocruz@gmail.com>
2024-10-08 14:45:01 +0200

    hahsdahdha

---
+++ Src/02.Sobrevida.R
@@ -0,0 +1,25 @@
+######################
+library(survival)
+library(tidyverse)
+library(readxl)
+
+Arkansas <- read_excel("Input/Arkansas.xlsx", 
+                       col_types = c("numeric", "numeric", "text", 
+                                     "numeric", "text", "text", "text", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric"))
+
+
+
+modelo1 <- coxph(Surv(OS_Time, OS_Censor)~AGE+SEX+PROT+B2M, data=Arkansas)
+summary(modelo1)
+
+### Iteraciones
+modelo1$iter
+
+### Varianzas
+sqrt(modelo1$var)
